a0eab34848aa65b32e85f81e030fe28598d12a18
Hugo <hverger@wanadoo.fr>
2015-11-11 13:59:38 +0100

    Added comments for results

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,78 @@
 window.onload = function() {
 	if (localStorage.getItem(1337) != null)
 	{
-		var average = localStorage.getItem(1337) + "ms";
-		document.getElementById("result1").innerHTML = average;
+		var average = localStorage.getItem(1337);
+		if (average < 200)
+		{
+			$("#comment1").html("Very Good !");
+			$("#comment1").addClass("verygood");
+		}
+		else if (average < 250)
+		{
+			$("#comment1").html("Good");
+			$("#comment1").addClass("good");
+		}
+		else if (average < 350)
+		{
+			$("#comment1").html("Average");
+			$("#comment1").addClass("average");
+		}
+		else
+		{
+			$("#comment1").html("Bad");
+			$("#comment1").addClass("bad");
+		}
+		$("#result1").html(average + "ms");
 	}
 	if (localStorage.getItem(1338) != null)
 	{
 		var click = localStorage.getItem(1338)*5;
-		document.getElementById("result2").innerHTML = click;
+		if (click > 500)
+		{
+			$("#comment2").html("Very Good !");
+			$("#comment2").addClass("verygood");
+		}
+		else if (click > 350)
+		{
+			$("#comment2").html("Good");
+			$("#comment2").addClass("good");
+		}
+		else if (click > 200)
+		{
+			$("#comment2").html("Average");
+			$("#comment2").addClass("average");
+		}
+		else
+		{
+			$("#comment2").html("Bad");
+			$("#comment2").addClass("bad");
+		}
+		$("#result2").html(click);
 	}
 	if (localStorage.getItem(1339) != null)
 	{
-		var accuracy = localStorage.getItem(1339) + " targets";
-		document.getElementById("result3").innerHTML = accuracy;
+		var target = localStorage.getItem(1339)*(2.5);
+		if (target > 85)
+		{
+			$("#comment3").html("Very Good !");
+			$("#comment3").addClass("verygood");
+		}
+		else if (target > 70)
+		{
+			$("#comment3").html("Good");
+			$("#comment3").addClass("good");
+		}
+		else if (target > 50)
+		{
+			$("#comment3").html("Average");
+			$("#comment3").addClass("average");
+		}
+		else
+		{
+			$("#comment3").html("Bad");
+			$("#comment3").addClass("bad");
+		}
+		$("#result3").html(target);
 	}
 }
 
